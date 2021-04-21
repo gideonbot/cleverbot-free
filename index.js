afcfb89d55701879a55a2d2d0fcc2346430a42bf
MBR#0001 <buildmachineubuntu@gmail.com>
2021-04-21 19:46:39 +0200

    fix

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ module.exports = async (stimulus, context = [], language) => {
         .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36")
         .type("text/plain")
         .send(payload);
-
-    return decodeURIComponent(req.header["cboutput"]);
+        
+    return req.text.split("\r")[0];
 };
